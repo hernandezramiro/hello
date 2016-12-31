d9ba614a182b1bf1fe1ed97a02412c026683f45d
ramiro.hernandez <urcoelmaldito@gmail.com>
2016-12-30 18:01:07 -0600

    first commit 20161230

--- hello.go
+++ hello.go
@@ -3,6 +3,23 @@
 import "fmt"
 
 func main() {
-    fmt.Printf("hello, world\n")
+	var ret int
 	
+	/* calling a function to get max value */
+	ret = max(10,15)
+	fmt.Printf( "Max value is : %d\n", ret)
 }
+
+/* function returning the max between two numbers */
+func max(num1, num2 int)int{
+	/* local variable declaration */
+	var result int;
+	
+	if (num1 > num2) {
+		result = num1
+	} else {
+		result = num2
+	}
+	return result 
+}
+
